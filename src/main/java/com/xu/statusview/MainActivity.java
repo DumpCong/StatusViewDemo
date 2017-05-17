c18bc99051d1c458dbcfd63c2ac6a6c0126896f9
DumpCong <497184894@qq.com>
2017-05-17 17:15:34 +0800

    首次上传

---
+++ src/main/java/com/xu/statusview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xu.statusview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //gogogo public static void main
+        setContentView(R.layout.activity_main);
+    }
+}
